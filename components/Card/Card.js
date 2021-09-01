c793a679e1afc5c0fd45630dbae60a9b85d92157
Noah Lustig <noah.jlustig@gmail.com>
2021-08-31 17:55:14 -0700

    remove unneccessary imports

--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import { TitleText, BodyText, CardContainer, HeaderContainer, ContentContainer, ActionsContainer} from './Card.styles';
 import { StylesProvider } from '@material-ui/core/styles';
