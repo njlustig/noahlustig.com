afe3b31f1d585aef8da218e8bf8e310a1101c1c6
Noah Lustig <noah.jlustig@gmail.com>
2021-09-08 08:02:31 -0700

    run build

---
+++ out/_next/static/NikyGZo9JUy0DL4MUzMjP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/965-cc8cc28b81d859b5d705.js","static/chunks/pages/index-4a5e5d219b152094bdfb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
