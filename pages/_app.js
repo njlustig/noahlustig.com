5aeccf527b1b88dbe42e5cac6321eff9afcc6cc5
Noah Lustig <noah.jlustig@gmail.com>
2021-09-08 07:25:37 -0700

    add title component to head

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import React from 'react';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <>
+    <Head>
+      <title>Noah Lustig</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+  );
+};
 
 export default MyApp
